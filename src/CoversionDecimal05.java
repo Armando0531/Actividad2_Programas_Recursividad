b2546fbf253bc56e1dce5f9b7bdfb1619872ef33
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-10-06 17:46:56 -0500

    Se creo programa que muestre la coversion de un numero decimal a binario

---
+++ src/CoversionDecimal05.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Conversor{
+	public static void conversorDecimalBinario(double num,int n) {
+		int x=(int) (num%2);
+			double f=num/2;
+			if(f>=0.5) {
+			if(num%2==0 || num%2==1) {
+				n=(int) (num/2);
+				conversorDecimalBinario(n,0);
+				System.out.print(x);
+			}else {
+				conversorDecimalBinario(n,0);
+			}
+		}
+		
+	}
+}
+
+public class CoversionDecimal05 {
+
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Ingresa un numero: ");
+		double n=entrada.nextInt();
+		System.out.println("El numero convertido a binario es:");
+		Conversor.conversorDecimalBinario(n,0);
+
+	}
+
+}
